0c6aaa9e44b9c46136baecd964c40a17f6a39c2d
Geovani Paz <geovanipaz7@gmail.com>
2022-03-22 17:21:16 -0300

    criação do model

--- My_Blog_Project/App_Blog/models.py
+++ My_Blog_Project/App_Blog/models.py
@@ -1,3 +1,18 @@
+from pyexpat import model
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Blog(models.Model):
+    autor = models.ForeignKey(User, on_delete=models.CASCADE,
+                              related_name='post_autor')
+    blog_title = models.CharField(max_length=264, verbose_name="Put a Title")
+    slug = models.SlugField(max_length=264, unique=True)
+    blog_content = models.TextField(verbose_name="What is in on mind?")
+    blog_image = models.ImageField(upload_to='blog_images', 
+                                   verbose_name="Blog Image")
+    publish_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return self.blog_title
