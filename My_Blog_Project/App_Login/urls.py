e8ef4f1a4f0c32a31465a8325492f991f0e71dd7
Geovani Paz <geovanipaz7@gmail.com>
2022-03-25 14:18:22 -0300

    impl. logout

--- My_Blog_Project/App_Login/urls.py
+++ My_Blog_Project/App_Login/urls.py
@@ -8,5 +8,7 @@ app_name = 'App_Login'
 
 urlpatterns = [
     path('signup/', views.sign_up, name='signup'),
-    path('signin/', views.login_page, name='signin')
+    path('signin/', views.login_page, name='signin'),
+    path('logout/', views.logout_user, name='logout'),
+    path('profile/', views.login_profile, name='profile'),
 ]
--- My_Blog_Project/App_Login/views.py
+++ My_Blog_Project/App_Login/views.py
@@ -3,13 +3,15 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, authenticate, logout
 from django.shortcuts import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from App_Login.forms import SignUpForm
 # Create your views here.
 
 def sign_up(request):
-    form = UserCreationForm()
+    form = SignUpForm()
     registered = False
     if request.method == 'POST':
-        form = UserCreationForm(data=request.POST)
+        form = SignUpForm(data=request.POST)
         if form.is_valid():
             form.save()
             registered = True
@@ -30,4 +32,12 @@ def login_page(request):
                 login(request, user)
                 return HttpResponseRedirect(reverse('index'))
     return render(request,'App_Login/login.html', context={'form':form})
-        
+ 
+@login_required      
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('App_Login:signin')) 
+
+@login_required
+def login_profile(request):
+    return render(request,'App_Login/profile.html',context={})
