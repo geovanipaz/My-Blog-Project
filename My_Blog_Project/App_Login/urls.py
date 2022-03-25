93e74fc0da3128e1375c2b0da35b5b23bef5103e
Geovani Paz <geovanipaz7@gmail.com>
2022-03-25 10:43:10 -0300

    implementação signin

--- My_Blog_Project/App_Login/urls.py
+++ My_Blog_Project/App_Login/urls.py
@@ -4,6 +4,9 @@ from django import views
 from django.urls import path
 from . import views
 
+app_name = 'App_Login'
+
 urlpatterns = [
-    path('signup/', views.sign_up, name='signup')
+    path('signup/', views.sign_up, name='signup'),
+    path('signin/', views.login_page, name='signin')
 ]
