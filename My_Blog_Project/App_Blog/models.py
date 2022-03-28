ce9b2daa1a066a27f2148f9d2e8fba22e7d184b5
Geovani Paz <geovanipaz7@gmail.com>
2022-03-28 16:20:39 -0300

    listView

--- My_Blog_Project/App_Blog/models.py
+++ My_Blog_Project/App_Blog/models.py
@@ -14,6 +14,9 @@ class Blog(models.Model):
     publish_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
     
+    class Meta:
+        ordering = ['-publish_date']
+    
     def __str__(self) -> str:
         return self.blog_title
     
@@ -26,6 +29,8 @@ class Comment(models.Model):
     comment = models.TextField()
     comment_date = models.DateTimeField(auto_now_add=True)
     
+    class Meta:
+        ordering = ['-comment_date']
 class Likes(models.Model):
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE,
                              related_name='liked_blog')
--- My_Blog_Project/App_Blog/views.py
+++ My_Blog_Project/App_Blog/views.py
@@ -6,14 +6,29 @@ from .models import Blog,Comment, Likes
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+import uuid
 # Create your views here.
 
 def blog_list(request):
     dict = {}
     return render(request, 'App_Blog/blog_list.html', context=dict)
 
+class BlogList(ListView):
+    context_object_name = 'blogs'
+    model = Blog
+    template_name = 'App_Blog/blog_list.html'
+    queryset = Blog.objects.order_by('-publish_date')
+
+
 class CreateBlog(CreateView, LoginRequiredMixin):
     model = Blog
     template_name = 'App_Blog/create_blog.html'
-    fields = '__all__'
+    fields = ('blog_title','blog_content','blog_image')
+    
+    def form_valid(self, form):
+        blog_obg = form.save(commit=False)
+        blog_obg.autor = self.request.user
+        title = blog_obg.blog_title
+        blog_obg.slug = title.replace(" ","-")+ "-" +str(uuid.uuid4())
+        blog_obg.save()
+        return HttpResponseRedirect(reverse('index'))
